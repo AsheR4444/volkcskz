a512cde3ef4df3e7cef32e56b717b0d32a0731d3
Илья Тюрин <il08082000@gmail.com>
2019-12-02 17:55:31 +0700

    Добавлены экраны (дилер/новости/акции/модели) без адаптива

--- js/js.js
+++ js/js.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
     prevArrow: $('.slider__arrow_left'),
     nextArrow: $('.slider__arrow_right')
   });
+  $('.models-slider').slick({
+    arrows: true,
+    slidesToShow: 4,
+    dots: true
+  });
+  $('.offer-slider').slick({
+    arrows: true,
+    slidesToShow: 3,
+    dots: true
+  });
 });
